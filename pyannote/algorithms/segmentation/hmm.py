908ff7878faec99b8115a6e2d1c1e236f5a48910
Herve Bredin <bredin@limsi.fr>
2014-11-24 09:19:34 +0100

    feat: Viterbi-GMMUBM

--- pyannote/algorithms/segmentation/hmm.py
+++ pyannote/algorithms/segmentation/hmm.py
@@ -31,6 +31,130 @@ from ..stats.lbg import LBG
 from ..utils.viterbi import viterbi_decoding, VITERBI_CONSTRAINT_NONE
 from pyannote.core import Annotation
 from pyannote.core.util import pairwise
+import sklearn
+from ..utils.sklearn_io import SKLearnIOMixin
+from ..classification.gmm import _GMMUBMClassification
+
+
+class _ViterbiGMMUBMSegmentation(_GMMUBMClassification):
+
+    def predict(self, X, constraint=None):
+
+        posteriors = self.predict_proba(X)
+
+        sequence = viterbi_decoding(
+            np.log(posteriors), self.transition_,
+            initial=self.initial_,
+            consecutive=self.consecutive_, constraint=constraint)
+
+        return sequence
+
+
+class ViterbiGMMUBMSegmentation(_ViterbiGMMUBMSegmentation, SKLearnIOMixin):
+
+    # TODO: speaker priors
+    # TODO: open-set speaker ID (unknown prior based on Unknown segments)
+
+    def __init__(self, n_jobs=1, n_components=1, covariance_type='diag',
+                 random_state=None, thresh=1e-2, min_covar=1e-3,
+                 n_iter=100, n_init=1, params='wmc', init_params='wmc',
+                 adapt_iter=10, adapt_params='m', consecutive=None):
+
+        super(_ViterbiGMMUBMSegmentation, self).__init__(
+            n_jobs=n_jobs, n_components=n_components, covariance_type=covariance_type,
+            random_state=random_state, thresh=thresh, min_covar=min_covar,
+            n_iter=n_iter, n_init=n_init, params=params, init_params=init_params,
+            adapt_iter=adapt_iter, adapt_params=adapt_params)
+
+        self.consecutive = consecutive
+
+    def train(self, annotation_iter, features_iter):
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # training data
+        X, y = [], []
+        for _X, _y in self.Xy_iter(annotation_iter, features_iter):
+            X.append(_X)
+            y.append(_y)
+        X = np.vstack(X)
+        Y = np.hstack(y)  # note: y is used later for transition probabilities
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # GMM/UBM training
+        self.fit(X, Y)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # number of classes
+        K = len(self.label_binarizer_.classes_)
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # initial & transition probabilities
+
+        initial = np.zeros((K, ), dtype=float)
+        transition = np.zeros((K, K), dtype=float)
+
+        for _y in y:
+            # convert sequence from label to class
+            sequence = np.where(self.label_binarizer_.transform(_y))[1]
+
+            # increment initial count
+            initial[sequence[0]] += 1
+
+            # increment transition counts
+            for n, m in pairwise(sequence):
+                transition[n, m] += 1
+
+        # log-probabilities
+        self.initial_ = np.log(initial / np.sum(initial))
+        self.transition_ = np.log(transition.T / np.sum(transition, axis=1)).T
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        return self
+
+    def apply(self, features, constraint=None):
+
+        # states and labels
+        labels = self.label_binarizer_.labels
+        K = len(labels)
+        state2label = {stt: lbl for stt, lbl in enumerate(labels)}
+        label2state = {lbl: stt for stt, lbl in enumerate(labels)}
+
+        # features
+        X = self.X(features)
+        N = X.shape[0]
+        sliding_window = features.sliding_window
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # mandatory/forbidden state constraints
+        constraint_ = VITERBI_CONSTRAINT_NONE * np.ones((N, K), dtype=int)
+        if constraint is not None:
+            for segment, _, label, value in constraint.itervalues():
+                if label not in label2state:
+                    continue
+                t, dt = sliding_window.segmentToRange(segment)
+                constraint_[t:t + dt, label2state[label]] = value
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # consecutive state constraints
+        consecutive = np.ones((K, ), dtype=int)
+        if isinstance(self.consecutive, dict):
+            for i, label in enumerate(labels):
+                duration = self.consecutive.get(label, 0.)
+                if duration > 0.:
+                    consecutive[i] = sliding_window.durationToSamples(duration)
+
+        elif self.consecutive is not None:
+            duration = float(self.consecutive)
+            consecutive[:] = sliding_window.durationToSamples(duration)
+
+        self.consecutive_ = consecutive
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        sequence = self.predict(X, constraint=constraint_)
+
+        return self.y2annotation(sequence, sliding_window, labels=state2label)
 
 
 class ViterbiHMM(object):
@@ -87,7 +211,8 @@ class ViterbiHMM(object):
     def __init__(self, targets=None, min_duration=0.,
                  n_components=1, covariance_type='diag',
                  random_state=None, thresh=1e-2, min_covar=1e-3, n_iter=10,
-                 disturb=0.05, sampling=0):
+                 disturb=0.05, sampling=0,
+                 ubm=None, params='m'):
         super(ViterbiHMM, self).__init__()
 
         self.targets = targets
@@ -101,13 +226,15 @@ class ViterbiHMM(object):
         self.disturb = disturb
         self.sampling = sampling
 
+        self.ubm = ubm
+        self.params = params
+
         self._lbg = LBG(n_components=self.n_components,
                         covariance_type=self.covariance_type,
                         random_state=self.random_state, thresh=self.thresh,
                         min_covar=self.min_covar, n_iter=self.n_iter,
                         disturb=self.disturb, sampling=self.sampling)
 
-    #  in BaseClassification
     def _get_targets(self, annotation_iterator):
         """Get sorted list of targets from training data"""
 
@@ -117,7 +244,6 @@ class ViterbiHMM(object):
 
         return sorted(targets)
 
-    # in BaseClassification
     def _get_target_data(self, annotation_iterator, features_iterator, target):
         """Get training data for state `target`"""
 
@@ -169,6 +295,42 @@ class ViterbiHMM(object):
 
         return np.log(1. * transition.T / np.sum(transition, axis=1)).T
 
+    def _adapt_ubm(self, ubm, data):
+        """Adapt UBM to new data using the EM algorithm
+
+        Parameters
+        ----------
+        data : array_like, shape (n, n_features)
+            List of n_features-dimensional data points.  Each row
+            corresponds to a single data point.
+
+        Returns
+        -------
+        gmm : GMM
+            Adapted UBM
+
+        """
+
+        # copy UBM structure and parameters
+        gmm = sklearn.clone(ubm)
+        gmm.params = self.params  # only adapt requested parameters
+        gmm.n_iter = self.n_iter
+        gmm.n_init = 1
+        gmm.init_params = ''      # initialize with UBM attributes
+
+        # initialize with UBM attributes
+        gmm.weights_ = ubm.weights_
+        gmm.means_ = ubm.means_
+        gmm.covars_ = ubm.covars_
+
+        # adaptation
+        try:
+            gmm.fit(data)
+        except ValueError, e:
+            logging.error(e)
+
+        return gmm
+
 
     def _fit_model(self, data):
         """Fit GMM to `data`"""
@@ -200,7 +362,26 @@ class ViterbiHMM(object):
 
         # train target models
         self._model = {}
+
+
+        for targets in self.ubm:
+
+            # gather data from all targets in group
+            data = np.vstack([
+                self._get_target_data(A, F, target)
+                for target in targets])
+
+            # train UBM
+            ubm = self._fit_model(data)
+
+            # adapt UBM to all targets in group
+            for target in targets:
+                data = self._get_target_data(A, F, target)
+                self._model[target] = self._adapt_ubm(ubm, data)
+
         for target in self.targets:
+            if target in self._model:
+                continue
             data = self._get_target_data(A, F, target)
             self._model[target] = self._fit_model(data)
 
