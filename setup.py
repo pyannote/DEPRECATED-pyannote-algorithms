aaa9e4bfe156eb2444bd5d9c02575cabe5f0d974
Hervé BREDIN <bredin@limsi.fr>
2016-02-12 15:07:36 +0100

    fix: add missing networkx dependency

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'pyannote.parser >= 0.4.2',
         'scikit-learn >= 0.17',
         'scipy >=0.17.0',
+        'networkx >= 1.11',
         'munkres >= 1.0.7',
         'docopt >= 0.6.2',
         'joblib >= 0.9.4',
