aab5073c0179226a17e1d15781d9c510c7deaead
Hervé BREDIN <bredin@limsi.fr>
2016-02-12 14:39:58 +0100

    style: PEP8

--- pyannote/algorithms/clustering/hac/model.py
+++ pyannote/algorithms/clustering/hac/model.py
@@ -12,8 +12,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -25,9 +25,7 @@
 
 from __future__ import unicode_literals
 
-"""Models for hierarchical agglomerative clustering
-
-"""
+"""Models for hierarchical agglomerative clustering"""
 
 import numpy as np
 from itertools import combinations, product
